86c4eba54b728856ff76b013ee211d806bac7020
ToiLaKiet <tuilakietday05@gmail.com>
2025-09-10 16:03:18 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css'
 import hackathonGraphic from './assets/hackathon-graphic.svg'
-import naverLogo from './assets/naver-logo.svg'
+import TaskFlow from './assets/logo_without_title.svg'
+import Header from './components/header/header.tsx'
 
 function App() {
   return (
     <div className="container">
+      <Header/>
       <div className="content">
-        <img src={naverLogo} alt="NAVER Vietnam AI Hackathon" className="logo" />
-        
-        <div className="greeting">
-          <p className="hello">Xin chào! 안녕하세요!</p>
-          <p className="subtitle">Hello World</p>
+          <img src={TaskFlow} alt="NAVER Vietnam AI Hackathon" className="logo" />
+          <div className="greeting">
+            <p className="hello">Xin chào! 안녕하세요!</p>
+            <p className="subtitle">Hello World</p>
         </div>
       </div>
-      
       <img className="graphic" src={hackathonGraphic} alt="" />
     </div>
   )
